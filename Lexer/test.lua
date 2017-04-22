2f40548dcb27fde0be94ff772433e59a682e7e0c
Oliver Cooper <oliver.cooper@me.com>
2017-04-22 19:22:04 +1200

    Added enum

--- Lexer/test.lua
+++ Lexer/test.lua
@@ -1,5 +1,81 @@
 lexer = require "lexer"
 
+
+
+local g_tLuaKeywords = {
+    [ "and" ] = true,
+    [ "break" ] = true,
+    [ "do" ] = true,
+    [ "else" ] = true,
+    [ "elseif" ] = true,
+    [ "end" ] = true,
+    [ "false" ] = true,
+    [ "for" ] = true,
+    [ "function" ] = true,
+    [ "if" ] = true,
+    [ "in" ] = true,
+    [ "local" ] = true,
+    [ "nil" ] = true,
+    [ "not" ] = true,
+    [ "or" ] = true,
+    [ "repeat" ] = true,
+    [ "return" ] = true,
+    [ "then" ] = true,
+    [ "true" ] = true,
+    [ "until" ] = true,
+    [ "while" ] = true,
+}
+
+-- A modified textutils.serialise
+function serialise( t, tTracking, sIndent )
+	tTracking = tTracking or {}
+	sIndent = sIndent or ""
+    local sType = type(t)
+    if sType == "table" then
+        if tTracking[t] ~= nil then
+            error( "Cannot serialize table with recursive entries", 0 )
+        end
+        tTracking[t] = true
+
+        if next(t) == nil then
+            -- Empty tables are simple
+            return "{}"
+        else
+            -- Other tables take more work
+            local sResult = "{\n"
+            local sSubIndent = sIndent .. "  "
+            local tSeen = {}
+            for k,v in ipairs(t) do
+                tSeen[k] = true
+                sResult = sResult .. sSubIndent .. serialise( v, tTracking, sSubIndent ) .. ",\n"
+            end
+            for k,v in pairs(t) do
+                if not tSeen[k] then
+                    local sEntry
+                    if type(k) == "string" and not g_tLuaKeywords[k] and string.match( k, "^[%a_][%a%d_]*$" ) then
+                        sEntry = k .. " = " .. serialise( v, tTracking, sSubIndent ) .. ",\n"
+                    else
+                        sEntry = "[ " .. serialise( k, tTracking, sSubIndent ) .. " ] = " .. serialise( v, tTracking, sSubIndent ) .. ",\n"
+                    end
+                    sResult = sResult .. sSubIndent .. sEntry
+                end
+            end
+            sResult = sResult .. sIndent .. "}"
+            return sResult
+        end
+        
+    elseif sType == "string" then
+        return string.format( "%q", t )
+    
+    elseif sType == "number" or sType == "boolean" or sType == "nil" then
+        return tostring(t)
+        
+    else
+        error( "Cannot serialize type "..sType, 0 )
+        
+    end
+end
+
 local h = io.open("Example.luo", "r")
 local line = h:read()
 local str = ""
@@ -8,6 +84,5 @@ while line do
 	line = h:read()
 end
 
-for k,v in lexer.lua(str) do
-	print(k,v)
+for k,v in ipairs(lexer.lex(str)) do
 end
