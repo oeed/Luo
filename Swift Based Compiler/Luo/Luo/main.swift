e41d0012d7877dbe0212ea5b511a30087eaca93b
Oliver Cooper <oliver.cooper@me.com>
2017-04-22 19:21:31 +1200

    Started the Swift based compiler
    Using Swift makes it easier to convert the compiler across to Luo. The
    way the compiler would be structured in Swift is far more alike it
    would in Luo than using Lua.

---
+++ Swift Based Compiler/Luo/Luo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Luo
+//
+//  Created by Oliver Cooper on 22/04/17.
+//  Copyright © 2017 Oliver Cooper. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
