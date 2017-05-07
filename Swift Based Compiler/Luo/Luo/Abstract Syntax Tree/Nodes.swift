cde60f9bb8265917b28dab04fc7357f84a136f1f
oeed <oliver.cooper@me.com>
2017-05-07 15:18:23 +1200

    Added remaining nodes

--- Swift Based Compiler/Luo/Luo/Abstract Syntax Tree/Nodes.swift
+++ Swift Based Compiler/Luo/Luo/Abstract Syntax Tree/Nodes.swift
@@ -11,13 +11,17 @@ import Foundation
 
 enum Node {
 	
-	case block(Block)
-	case identifier(Identifier)
-	case assignee(Assignable)
+	case block(Block, Position)
+    case statement(Block, Position)
+    case expression(Expression, Position)
+    case apply(Appliable, Position)
+    case identifier(Identifier, Position)
+    case assignee(Assignable, Position)
 
 }
 
 typealias Block = [Statement]
+typealias Label = String
 indirect enum Statement {
 	
 	case `do`(statements: [Statement])
@@ -29,9 +33,15 @@ indirect enum Statement {
 	case forIn(variables: [Identifier], iterators: [Identifier], Block)
 	case local(variables: [Identifier], value: [Expression])
 	case localFunction(name: Identifier, block: Block)
+    case goto(label: Label)
+    case label(Label)
+    case `return`([Expression])
+    case `break`
+    case apply(Appliable)
+    
 }
 
-enum Expression {
+indirect enum Expression {
 	
 	case `nil`
 	case dots
@@ -39,11 +49,69 @@ enum Expression {
 	case `false`
 	case number(Double)
 	case string(String)
-	case function([Identifier], isVarArg: Bool)
+    case function([Identifier], isVarArg: Bool)
+    case `operator`(NodeOperator, Expression, Expression?)
+    case table([TableItem]) // table constructor
+    case brackets(Expression) // i.e. print((unpack {1, 2, 3})) only prints one, wrapping brackets only gives the first return value
+    case apply(Appliable)
+    case assignable(Assignable)
 	
 }
 
+enum NodeOperator {
+    
+    case doubleEqual
+    case notEqual
+    case lessThanEqual
+    case greaterThanEqual
+    case concatenate
+    case plusPlus
+    case minusMinus
+    case plusEqual
+    case minusEqual
+    case multiplyEqual
+    case divideEqual
+    case modulusEqual
+    case exponentEqual
+    case equal
+    case plus
+    case multiply
+    case minus
+    case hash
+    case divide
+    case modulus
+    case exponent
+    case greatherThan
+    case lessThan
+    case and
+    case not
+    
+}
+
+enum TableItem {
+    
+    case array(value: Expression) // an array item, added like { a, b }
+    case dictionary(key: Expression, value: Expression) // a dictionary item, added like { [a] = b }
+    
+}
+
 typealias Identifier = String
+
+protocol Appliable {}
+struct Call: Appliable {
+    
+    let callee: Expression
+    let arguments: [Expression]
+    
+}
+
+struct Invocation {
+ 
+    let callee: Expression
+    let method: String
+    let arguments: [Expression]
+    
+}
 
 protocol Assignable {}
 extension Identifier: Assignable {}
