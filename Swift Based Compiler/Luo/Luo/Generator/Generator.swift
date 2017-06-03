7a5c8c1d85ed2f2ef23b350e4d1ec46f1f35c0eb
Oliver Cooper <oliver.cooper@me.com>
2017-06-03 19:11:55 +1200

    Started generator

---
+++ Swift Based Compiler/Luo/Luo/Generator/Generator.swift
@@ -0,0 +1,13 @@
+//
+//  Writer.swift
+//  Luo
+//
+//  Created by Oliver Cooper on 3/06/17.
+//  Copyright © 2017 Oliver Cooper. All rights reserved.
+//
+
+import Foundation
+
+protocol Generator {
+	
+}
---
+++ Swift Based Compiler/Luo/Luo/Generator/LuaGenerator.swift
@@ -0,0 +1,9 @@
+//
+//  LuaOutputter.swift
+//  Luo
+//
+//  Created by Oliver Cooper on 3/06/17.
+//  Copyright © 2017 Oliver Cooper. All rights reserved.
+//
+
+import Foundation
